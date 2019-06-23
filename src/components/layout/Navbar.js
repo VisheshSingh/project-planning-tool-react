fc8f40c7fc0123dc1b553327b574d64df0d65cbe
singhv <vishesh.singh@biworldwide.com>
2019-06-23 18:55:11 -0500

    planning and structuring

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="nav-wrapper">
+        <div className="container">
+          <a href="!#" className="brand-logo">
+            MarioPlan
+          </a>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
