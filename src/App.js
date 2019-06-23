da1f99630cd0c625c9f3a8802a814167af04ce82
singhv <vishesh.singh@biworldwide.com>
2019-06-23 18:58:32 -0500

    Raect-Router package

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Navbar from './components/layout/Navbar';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+      </div>
+    </BrowserRouter>
   );
 }
 
