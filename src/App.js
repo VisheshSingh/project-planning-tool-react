4340475ad67235ad3d71602045de9a70d745df46
singhv <vishesh.singh@biworldwide.com>
2019-06-23 22:37:27 -0500

    sign up component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
 import ProjectDetails from './components/projects/ProjectDetails';
 import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route exact path="/" component={Dashboard} />
           <Route path="/project/:id" component={ProjectDetails} />
           <Route path="/login" component={SignIn} />
+          <Route path="/signup" component={SignUp} />
         </Switch>
       </div>
     </BrowserRouter>
---
+++ src/components/auth/SignUp.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+
+class SignUp extends Component {
+  state = {
+    firstname: '',
+    lastname: '',
+    email: '',
+    password: ''
+  };
+  handleChange = e => {
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+  render() {
+    return (
+      <div className="container">
+        <form onSubmit={this.handleSubmit} className="white">
+          <h5 className="grey-text text-darken-3">Sign Up</h5>
+          <div className="row">
+            <div className="col s12 m5">
+              <div className="input-field">
+                <label htmlFor="firstname">First Name</label>
+                <input
+                  type="text"
+                  id="firstname"
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className="input-field">
+                <label htmlFor="lastname">Last Name</label>
+                <input type="text" id="lastname" onChange={this.handleChange} />
+              </div>
+            </div>
+            <div className="col s12 m5 offset-m2">
+              <div className="input-field">
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" onChange={this.handleChange} />
+              </div>
+              <div className="input-field">
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  id="password"
+                  onChange={this.handleChange}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="input-field center">
+            <button className="btn z-depth-0 pink">Sign Up</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignUp;
