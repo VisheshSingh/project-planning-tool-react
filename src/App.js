96c4b37ad691286d517fa2f3d7fdc13abf4da628
singhv <vishesh.singh@biworldwide.com>
2019-06-23 22:13:57 -0500

    SignIn component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
 import ProjectDetails from './components/projects/ProjectDetails';
+import SignIn from './components/auth/SignIn';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Dashboard} />
           <Route path="/project/:id" component={ProjectDetails} />
+          <Route path="/login" component={SignIn} />
         </Switch>
       </div>
     </BrowserRouter>
