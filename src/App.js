4788b3365cf095af41c8b330a0eb3b3d05d4e3b7
singhv <vishesh.singh@biworldwide.com>
2019-06-23 19:29:49 -0500

    Dashboard component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import { BrowserRouter } from 'react-router-dom';
+import Dashboard from './components/dashboard/Dashboard';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar />
+        <Route path="/" component={Dashboard} />
       </div>
     </BrowserRouter>
   );
---
+++ src/components/dashboard/Notifications.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Notifications() {
+  return <div className="notifications container">Notifications</div>;
+}
+
+export default Notifications;
