88387125f6148b7a9c1d2140e86a03264c33ef58
singhv <vishesh.singh@biworldwide.com>
2019-06-24 23:54:24 -0500

    connecting redux to firestore

--- src/actions/createProject.js
+++ src/actions/createProject.js
@@ -1,5 +1,5 @@
 export const createProject = project => {
-  return (dispatch, getState) => {
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
     // make async call
     dispatch({ type: 'CREATE', payload: project });
   };
--- src/index.js
+++ src/index.js
@@ -7,11 +7,18 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers/rootReducer';
 import thunk from 'redux-thunk';
+import { reduxFirestore, getFirestore } from 'redux-firestore';
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
+import fbConfig from './config/fbConfig';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
+  composeEnhancers(
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+    reduxFirestore(fbConfig),
+    reactReduxFirebase(fbConfig)
+  )
 );
 
 ReactDOM.render(
