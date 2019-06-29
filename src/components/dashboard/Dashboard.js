4e1c73f9695f1add5058fab7c7ac2e755f039734
VisheshSingh <visheshsingh16@gmail.com>
2019-06-29 16:21:19 -0500

    clean up logs

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -23,7 +23,6 @@ class Dashboard extends Component {
 }
 
 const mapStateToProps = state => {
-  console.log(state);
   return {
     projects: state.firestore.ordered.projects || state.project.projects
   };
