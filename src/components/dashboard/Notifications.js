7c54fb6966c86e02e4f42559769a3ba786dab37a
VisheshSingh <visheshsingh16@gmail.com>
2019-06-30 12:14:55 -0500

    Notification component

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
 function Notifications() {
-  return <div className="notifications container">Notifications</div>;
+  return (
+    <div className="section">
+      <div className="card z-depth-0">
+        <div className="card-content">
+          <span className="card-title">Notifications</span>
+          <ul className="notifications">
+            <li>Notification</li>
+            <li>Notification</li>
+            <li>Notification</li>
+            <li>Notification</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Notifications;
